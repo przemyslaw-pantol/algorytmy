cd9285c0ffbac297ebd6cd84a07ea1ef767c3187
Jakub-Marecki <jakubmarecki22@gmail.com>
2024-11-12 11:17:34 +0100

    cokolwiek

--- szukanie_metody.py
+++ szukanie_metody.py
@@ -1,5 +1,10 @@
 import time
-
+'''
+Dekorator mierzący czas wykonania funkcji.
+Działa, opakowując oryginalną funkcję w dodatkową logikę mierzenia czasu.
+Po wywoładniu funkcji zapisuje czas rozpoczęcia i zakończenia, a następnie oblicza różnicę.
+Zwraca listę zawierającą wynik funkcji oraz czas wykonania.
+'''
 def time_mesure(func):
     def wrapper(*args, **kwargs):
         start=time.perf_counter()
@@ -8,6 +13,15 @@ def time_mesure(func):
         return [results,end-start]
     return wrapper
 
+'''
+Funkcja realizujca wyszukiwanie liniowe.
+Przeszukuje listę od początku do końca, porównując każdy element z wartością, której szukamy.
+-jeśli znajdzie element pasujący, zwraca jego indeks.
+-jeśli nie znajdzie, zwraca None.
+
+Wyszukiwanie liniowe jest prostym algorytmem, ale może być nieefektywne 
+przy dużych listach, ponieważz sprawdza każdy element po kolei.
+'''
 @time_mesure
 def linear_search(input_list, value):
     i = 0  
@@ -16,7 +30,17 @@ def linear_search(input_list, value):
             return i  
         i += 1  
     return None  
+'''
+Funkcja realizująca wyszukiwanie binarne.
+Działa na posortowanej liście, co pozwala na szybsze znalezienie wartości.
+W każdym kroku dzieli zakres poszukiwań na pół:
+- jeśli wartość poszukiwana jest rónwa środkowemu elementowi, zwraca liczbę iteracji.
+- jeśli wartość poszukiwana jest mniejsza, przeszukuje lewą połowę.
+- jeśli wartość poszukiwna jest większa, przeszukuje lewą połowę.
 
+Funkcja zwraca liczbę iteracji potrzebnych do znalezienia wartości lub None, jeśli wartość nie istnieje.
+
+'''
 @time_mesure
 def binary_search(input_list, value, start, end):
     i=0
@@ -24,16 +48,23 @@ def binary_search(input_list, value, start, end):
         i+=1
 
         mid = (start + end) // 2
-        mid_value=input_list[mid].strip()
 
-        if mid_value== value:
-            return  i
-        elif mid_value < value:
+        if input_list[mid].strip() == value:
+            return  int(i)
+        elif input_list[mid].strip() < value:
             start=mid + 1
         else:
             end=mid-1
     return None
 
+'''
+Funkcja grupująca elementy listy w słowniku na podstawie ich pierwszej litery.
+-Klucz w słowniku to pierwsza litera (np. 'a', 'b', ... )
+-Wartość to lista wszystkich elementów zaczynających się na tę literę.
+
+Funckja iteruje przez podaną listę, usuwa zbędne białe znaki i organizuje elementy w grupy.
+Przydaje się do szybkiego wyszukiwania elementów na podstawie ich pierwszej litery.
+'''
 def serching_dict(lista):
     dictionary = {}
     for name in lista:
@@ -44,7 +75,11 @@ def serching_dict(lista):
                 dictionary[first_letter] = []
             dictionary[first_letter].append(name)
     return dictionary
-
+'''
+Funkcja grupująca elementy listy w liście list, gdzie każda podlista odpowiada jednej literze alfabetu (a-z)
+- Tworzy 26 pustych list, odpowiadających literom 'a' do 'z'.
+- Dodaje każdy element listy wejściowej do odpowiedniej podlisty na podstawie pierwszej litery eleemtnu
+'''
 def serching_list(lista):
     lista_koncowa=[[] for _ in range(26)]
     for name in lista:
--- szukanie_wizualizacja.py
+++ szukanie_wizualizacja.py
@@ -1,97 +1,124 @@
-from szukanie_metody import linear_search,binary_search,serching_dict,serching_list
+from szukanie_metody import linear_search, binary_search, serching_dict, serching_list
 import matplotlib.pyplot as plt
 import random
-from functools import reduce
 
+# Ścieżka do pliku z danymi tekstowymi
 FILE_PATH = "filtered_words.txt"
 
 def file_read(input_file):
+    """
+    Funkcja odczytująca dane z pliku tekstowego.
+    - Zwraca listę słów, usuwając białe znaki (spacje, nowe linie) z każdego słowa.
+    """
     with open(input_file, 'r') as file:
-        lista = file.read().splitlines()
-        lista = [word.strip() for word in lista]
+        lista = file.read().splitlines()  # Odczyt linii
+        lista = [word.strip() for word in lista]  # Usuwanie zbędnych znaków
     return lista
 
 def lista_filtered(char, list):
-    return  list[ord(char)-97]
+    """
+    Filtruje listę list, wybierając tę grupę, która zaczyna się od określonej litery.
+    - char: litera, na podstawie której filtrujemy.
+    - list: lista list grupowanych według pierwszej litery.
+    - Zwraca posortowaną podlistę lub False, jeśli brak pasującej grupy.
+    """
+    for group in list:
+        if group[0][0] == char:  # Szukanie grupy zaczynającej się na literę
+            group.sort()
+            return group
+    return False
 
-file = file_read(FILE_PATH)
-file.sort()
-lista_losowa = [random.choice(file) for _ in range(501)]  
-dictionary = serching_dict(file)
-lista_podzial = serching_list(file)
+# Przygotowanie danych
+file = file_read(FILE_PATH)  # Odczyt słów z pliku
+file.sort()  # Posortowanie listy słów
+lista_losowa = [random.choice(file) for _ in range(501)]  # Generowanie losowych słów z pliku
+dictionary = serching_dict(file)  # Grupowanie słów w słowniku
+lista_podzial = serching_list(file)  # Grupowanie słów w liście list
 
+# Tablice sumujące wyniki wyszukiwań i czasów
+total_binarne = [0, 0, 0]  # Iteracje dla różnych wersji wyszukiwania binarnego
+total_liniowe = [0, 0, 0]  # Iteracje dla różnych wersji wyszukiwania liniowego
+total_binarne_czas = [0, 0, 0]  # Czas dla różnych wersji wyszukiwania binarnego
+total_liniowe_czas = [0, 0, 0]  # Czas dla różnych wersji wyszukiwania liniowego
+wariancja_iter_binarne = []  # Iteracje dla każdego wyszukiwania binarnego
+wariancja_iter_liniowe = []  # Iteracje dla każdego wyszukiwania liniowego
 
-total_binarne = [[],[],[]]  
-total_liniowe = [[],[],[]]  
-total_binarne_czas = [[],[],[]]  
-total_liniowe_czas = [[],[],[]] 
-
-
+# Pętla testująca efektywność wyszukiwania na losowych słowach
 for WORD in lista_losowa:
-    dictionary_filtered = (dictionary.get(WORD[0]))
-    lista_filter = (lista_filtered(WORD[0], lista_podzial))
+    dictionary_filtered = (dictionary.get(WORD[0]))  # Słownik dla danej litery
+    lista_filter = (lista_filtered(WORD[0], lista_podzial))  # Lista z grupą dla danej litery
     
-    binarne_bez,time_bin_bez = binary_search(file, WORD, 0, len(file) - 1)
-    liniowe_bez,time_lin_bez = linear_search(file, WORD)
-    binarne_lista,time_bin_lis = binary_search(lista_filter, WORD, 0, len(lista_filter) - 1)
-    liniowe_lista,time_lin_lis = linear_search(lista_filter, WORD)
-    binarne_dict,time_bin_dic = binary_search(dictionary_filtered, WORD, 0, len(dictionary_filtered) - 1)
-    liniowe_dict,time_lin_dic = linear_search(dictionary_filtered, WORD)
+    # Testy wyszukiwania binarnego i liniowego
+    binarne_bez, time_bin_bez = binary_search(file, WORD, 0, len(file) - 1)
+    liniowe_bez, time_lin_bez = linear_search(file, WORD)
+    binarne_lista, time_bin_lis = binary_search(lista_filter, WORD, 0, len(lista_filter) - 1)
+    liniowe_lista, time_lin_lis = linear_search(lista_filter, WORD)
+    binarne_dict, time_bin_dic = binary_search(dictionary_filtered, WORD, 0, len(dictionary_filtered) - 1)
+    liniowe_dict, time_lin_dic = linear_search(dictionary_filtered, WORD)
 
-    total_binarne[0].append(binarne_bez)
-    total_binarne[1].append(binarne_lista)
-    total_binarne[2].append(binarne_dict)
+    # Zbieranie danych iteracji
+    total_binarne[0] += binarne_bez
+    total_binarne[1] += binarne_lista
+    total_binarne[2] += binarne_dict
+    wariancja_iter_binarne.append(binarne_bez)
 
-    total_liniowe[0].append(liniowe_bez)
-    total_liniowe[1].append(liniowe_lista)
-    total_liniowe[2].append(liniowe_dict)
+    total_liniowe[0] += liniowe_bez
+    total_liniowe[1] += liniowe_lista
+    total_liniowe[2] += liniowe_dict
+    wariancja_iter_liniowe.append(liniowe_bez)
 
-    total_binarne_czas[0].append(time_bin_bez) 
-    total_binarne_czas[1].append(time_bin_lis)
-    total_binarne_czas[2].append(time_bin_dic)
+    # Zbieranie danych czasów
+    total_binarne_czas[0] += time_bin_bez
+    total_binarne_czas[1] += time_bin_lis
+    total_binarne_czas[2] += time_bin_dic
 
-    total_liniowe_czas[0].append(time_lin_bez)
-    total_liniowe_czas[1].append(time_lin_lis)
-    total_liniowe_czas[2].append(time_lin_dic)
+    total_liniowe_czas[0] += time_lin_bez
+    total_liniowe_czas[1] += time_lin_lis
+    total_liniowe_czas[2] += time_lin_dic
 
-binarne = [reduce(lambda x,y:x+y,x,0)/500 for x in total_binarne]
-liniowe = [reduce(lambda x,y:x+y,x,0)/500 for x in total_liniowe]
-binarne_czas = [reduce(lambda x,y:x+y,x,0)/500 for x in total_binarne_czas]
-liniowe_czas = [reduce(lambda x,y:x+y,x,0)/500 for x in total_liniowe_czas]
+# Obliczanie średnich wyników iteracji i czasów
+binarne = [x / 500 for x in total_binarne]
+liniowe = [x / 500 for x in total_liniowe]
+binarne_czas = [x / 500 for x in total_binarne_czas]
+liniowe_czas = [x / 500 for x in total_liniowe_czas]
 
-lab_binarne=['Binarne_bez_podziału','Binarne_lista','Binarne_slownik']
-lab_liniowe=['Liniowe_bez_podziału','Liniowe_lista','Liniowe_slownik']
+# Etykiety dla wykresów
+lab_binarne = ['Binarne_bez_podziału', 'Binarne_lista', 'Binarne_slownik']
+lab_liniowe = ['Liniowe_bez_podziału', 'Liniowe_lista', 'Liniowe_slownik']
 
+# Tworzenie wykresów
 fig, axs = plt.subplots(3, 2, figsize=(12, 12))
 
+# Wykres 1: Średnia liczba iteracji dla wyszukiwania binarnego
 axs[0, 0].bar(lab_binarne, binarne, color='purple')
 axs[0, 0].set_title("Szukanie binarne iteracje")
+axs[0, 0].bar_label(axs[0, 0].containers[0])
 
-
-axs[0, 0].bar_label(axs[0, 0].containers[0])  
-
+# Wykres 2: Średnia liczba iteracji dla wyszukiwania liniowego
 axs[0, 1].bar(lab_liniowe, liniowe, color='red')
 axs[0, 1].set_title("Szukanie liniowe iteracje")
+axs[0, 1].bar_label(axs[0, 1].containers[0])
 
-axs[0, 1].bar_label(axs[0, 1].containers[0])  
-
+# Wykres 3: Średni czas wyszukiwania binarnego
 bars_binarne_czas = axs[1, 0].bar(lab_binarne, binarne_czas, color='lime')
 axs[1, 0].set_title("Szukanie binarne czas (s)")
+axs[1, 0].bar_label(bars_binarne_czas, labels=[str(round(value, 10)) for value in binarne_czas])
 
-
-axs[1, 0].bar_label(bars_binarne_czas, labels=[str(round(value, 10)) for value in binarne_czas])  
-
+# Wykres 4: Średni czas wyszukiwania liniowego
 bars_liniowe_czas = axs[1, 1].bar(lab_liniowe, liniowe_czas, color='pink')
 axs[1, 1].set_title("Szukanie liniowe czas (s)")
+axs[1, 1].bar_label(bars_liniowe_czas, labels=[str(round(value, 5)) for value in liniowe_czas])
 
+# Wykres 5: Wariancja liczby iteracji dla wyszukiwania binarnego
+axs[2, 0].set_title("Wariancja liczby iteracji - binarne")
+axs[2, 0].plot(wariancja_iter_binarne)
+axs[2, 0].plot([total_binarne[0] / 500 for _ in range(0, 501)], color="red")
 
-axs[1, 1].bar_label(bars_liniowe_czas, labels=([str(round(value, 5)) for value in liniowe_czas]))
+# Wykres 6: Wariancja liczby iteracji dla wyszukiwania liniowego
+axs[2, 1].set_title("Wariancja liczby iteracji - liniowe")
+axs[2, 1].plot(wariancja_iter_liniowe)
+axs[2, 1].plot([total_liniowe[0] / 500 for _ in range(0, 501)], color="red")
 
-axs[2,0].plot(total_binarne[0])
-axs[2,0].plot([sum(total_binarne[0])/500 for _ in range(0,501)],color="red")
-
-axs[2,1].plot(total_liniowe[0])
-axs[2,1].plot([sum(total_liniowe[0])/500 for _ in range(0,501)],color="red")
-
-plt.subplots_adjust(hspace=0.3, bottom=0.038, top=0.966,left=0.03,right=0.97)
+# Dostosowanie wykresów
+plt.subplots_adjust(hspace=0.3, bottom=0.038, top=0.966, left=0.03, right=0.97)
 plt.show()
